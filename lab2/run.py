bba23e83f15ff310d909062e0b0e895d4701f6e4
tongzhen <120090694@link.cuhk.edu.cn>
2023-11-05 02:29:52 +0800

    lab2 doing closure

--- lab2/run.py
+++ lab2/run.py
@@ -1,5 +1,5 @@
 from utils import FunctionalDependency as FD
-
+from utils import Schema
 
 
 
@@ -11,17 +11,38 @@ if __name__ == "__main__":
     '''
     test 1 get closure
     '''
-    fd1 = FD(["Property_id#"], "County_name")
-    fd2 = FD(["Property_id#"], "Lot#")
-    fd3 = FD(["Property_id#"], "Area")
-    fd4 = FD(["Property_id#"], "Price")
-    fd5 = FD(["Property_id#"], "Tax_rate")
-    fd6 = FD(["County_name", "Lot#"], "Property_id#")
-    fd7 = FD(["County_name", "Lot#"], "Area")
-    fd8 = FD(["County_name", "Lot#"], "Price")
-    fd9 = FD(["County_name", "Lot#"], "Tax_rate")
-    fd10 = FD(["County_name"], "Tax_rate")
-    fd11 = FD(["Area"], "Price")
-    fds = [fd1, fd2, fd3, fd4, fd5, fd6, fd7, fd8, fd9, fd10, fd11]
+    # fd1 = FD(["Property_id#"], "County_name")
+    # fd2 = FD(["Property_id#"], "Lot#")
+    # fd3 = FD(["Property_id#"], "Area")
+    # fd4 = FD(["Property_id#"], "Price")
+    # fd5 = FD(["Property_id#"], "Tax_rate")
+    # fd6 = FD(["County_name", "Lot#"], "Property_id#")
+    # fd7 = FD(["County_name", "Lot#"], "Area")
+    # fd8 = FD(["County_name", "Lot#"], "Price")
+    # fd9 = FD(["County_name", "Lot#"], "Tax_rate")
+    # fd10 = FD(["County_name"], "Tax_rate")
+    # fd11 = FD(["Area"], "Price")
+    # fds = [fd1, fd2, fd3, fd4, fd5, fd6, fd7, fd8, fd9, fd10, fd11]
+    # emp_proj = Schema()
+    # for fd in fds:
+    #     fd.show()
+    #     emp_proj.add_FD(fd)
+    # print(emp_proj.attrs)
+
+    """
+    test 2 1NF
+    """
+    fd1 = FD(["Ssn", "Pnumber"], "Hours")
+    fd2 = FD(["Ssn"], "Ename")
+    fd3 = FD(["Pnumber"], "Pname")
+    fd4 = FD(["Pnumber"], "Plocation")
+    fds = [fd1, fd2, fd3, fd4]
+    emp_proj = Schema()
+    emp_proj.pk = ["Ssn", "Pnumber"]
     for fd in fds:
         fd.show()
+        emp_proj.add_FD(fd)
+    # print(emp_proj.attrs)
+    OneNFs = emp_proj.trans1NF()
+    for sch in OneNFs:
+        print(sch.attrs)
--- lab2/utils.py
+++ lab2/utils.py
@@ -20,30 +20,91 @@ class FunctionalDependency:
 
 class Schema:
 
-    def __init__(self, attrs:list, pk = None, FDs = None):
+    def __init__(self, pks = None):
         self.attrs = []
-        self.pk = pk
-        if FDs != None:
-            self.FDs = FDs
-        else:
-            self.FDs = []
+        self.pks = pks
+        self.FDs = []
+        self.F_plus = None
 
-    def add_FD(self, master_list:list, slave:str):
-        for master in master_list:
+    def add_FD(self, fd:FunctionalDependency):
+        for master in fd.master_attrs:
             if master not in self.attrs:
-                print("Error, add_FD() master not in attributes set")
-                return False
-        if slave not in self.attrs:
-            print("Error, add_FD() slave not in attributes set")
+                self.attrs.append(master)
+        if fd.slave not in self.attrs:
+            self.attrs.append(fd.slave)
+        self.FDs.append(fd)
+    
+    def trans1NF(self):
+        """
+        output: 1NF schema list
+        """
+        fd_kind_dict = {}
+        for fd in self.FDs:
+
+            # Check if the masters are all pks
+            if(is_subset(fd.master_attr, self.pks)):
+
+                master_name = ""
+                for master in fd.master_attrs:
+                    master_name += master
+                fd.show()
+                if master_name in fd_kind_dict.keys():
+                    fd_kind_dict[master_name].append(fd)
+                else:
+                    fd_kind_dict[master_name] = [fd]
+                    print("new masters = ", master_name)
+            
+            # Else, the msters have part not pks, find the master of the non-pks
+            else:
+                non_pk_masters = subtract_set(fd.master_attrs, self.pks)
+                new_masters = self.find_pks_masters(non_pk_masters) 
+
+        
+        oneNFs = []
+        for fds in fd_kind_dict.values():
+            sch = Schema()
+            for fd in fds:
+                sch.add_FD(fd)
+            oneNFs.append(sch)
+        return oneNFs
+
+    def find_pks_masters(self, non_pk_attrs):
+        """
+        input: a list of non-pk attributes
+        output a list of [pk masters list] (2-nest list)
+        """
+        pk_masters_set = []
+        for m in non_pk_attrs:
+            pk_masters = []
+            for fd in self.closure():
+                if m == fd.slave and is_subset(fd.master_attrs, self.pks):
+                    pk_masters+=fd.master_attrs
+                    break
+            pk_masters_set.append(pk_masters)
+        return pk_masters_set
+            
+    def closure(self, update = False):
+        """
+        output: the colsure list of it. 
+        """
+        
+        if self.F_plus == None or update:
+            # TODO get closure
+        
+        return self.F_plus
+
+
+
+def is_subset(l1:list, l2:list):
+    for i in l1:
+        if i not in l2:
             return False
-        FD = FunctionalDependency(master_list, slave)
-        self.FDs.append(FD)
+    return True
 
+def subtract_set(l1:list, l2:list):
+    l_subtract = []
+    for i in l1:
+        if i not in l2:
+            l_subtract.append(i)
+    return l_subtract
 
-def get_closure(FDs:list):
-    """
-    input: a list of Functional Dependency
-    output: the colsure list of it. 
-    """
-    for FD in FDs:
-        pass
