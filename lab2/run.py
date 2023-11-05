e699726c24788087360ede8dbde24042c6c02963
tongzhen <120090694@link.cuhk.edu.cn>
2023-11-05 23:23:37 +0800

    llab2 bug transitive augmentation bug

--- lab2/run.py
+++ lab2/run.py
@@ -8,20 +8,21 @@ from utils import Schema
 
 
 if __name__ == "__main__":
+    
     '''
-    test 1 get closure
+    test 1 get add_FD
     '''
-    # fd1 = FD(["Property_id#"], "County_name")
-    # fd2 = FD(["Property_id#"], "Lot#")
-    # fd3 = FD(["Property_id#"], "Area")
-    # fd4 = FD(["Property_id#"], "Price")
-    # fd5 = FD(["Property_id#"], "Tax_rate")
-    # fd6 = FD(["County_name", "Lot#"], "Property_id#")
-    # fd7 = FD(["County_name", "Lot#"], "Area")
-    # fd8 = FD(["County_name", "Lot#"], "Price")
-    # fd9 = FD(["County_name", "Lot#"], "Tax_rate")
-    # fd10 = FD(["County_name"], "Tax_rate")
-    # fd11 = FD(["Area"], "Price")
+    # fd1 = FD(["Property_id#"], ["County_name"])
+    # fd2 = FD(["Property_id#"], ["Lot#"])
+    # fd3 = FD(["Property_id#"], ["Area"])
+    # fd4 = FD(["Property_id#"], ["Price"])
+    # fd5 = FD(["Property_id#"], ["Tax_rate"])
+    # fd6 = FD(["County_name", "Lot#"], ["Property_id#"])
+    # fd7 = FD(["County_name", "Lot#"], ["Area"])
+    # fd8 = FD(["County_name", "Lot#"], ["Price"])
+    # fd9 = FD(["County_name", "Lot#"], ["Tax_rate"])
+    # fd10 = FD(["County_name"], ["Tax_rate"])
+    # fd11 = FD(["Area"], ["Price"])
     # fds = [fd1, fd2, fd3, fd4, fd5, fd6, fd7, fd8, fd9, fd10, fd11]
     # emp_proj = Schema()
     # for fd in fds:
@@ -30,19 +31,34 @@ if __name__ == "__main__":
     # print(emp_proj.attrs)
 
     """
-    test 2 1NF
+    test 2 get closure
     """
-    fd1 = FD(["Ssn", "Pnumber"], "Hours")
-    fd2 = FD(["Ssn"], "Ename")
-    fd3 = FD(["Pnumber"], "Pname")
-    fd4 = FD(["Pnumber"], "Plocation")
-    fds = [fd1, fd2, fd3, fd4]
-    emp_proj = Schema()
-    emp_proj.pk = ["Ssn", "Pnumber"]
+    fd1 = FD({"A"}, {"B"})
+    fd2 = FD({"B"}, {"C", "D"})
+    test2 = Schema()
+    fds = [fd1, fd2]
     for fd in fds:
-        fd.show()
-        emp_proj.add_FD(fd)
+        test2.add_FD(fd)
+    test2.closure()
+
+    '''
+    test 3 1NF
+    '''
+
+    # fd1 = FD(["Ssn", "Pnumber"], ["Hours"])
+    # fd2 = FD(["Ssn"], ["Ename"])
+    # fd3 = FD(["Pnumber"], ["Pname"])
+    # fd4 = FD(["Pnumber"], ["Plocation"])
+    # fds = [fd1, fd2, fd3, fd4]
+    # emp_proj = Schema()
+    # emp_proj.pk = ["Ssn", "Pnumber"]
+    # for fd in fds:
+        # fd.show()
+        # emp_proj.add_FD(fd)
     # print(emp_proj.attrs)
-    OneNFs = emp_proj.trans1NF()
-    for sch in OneNFs:
-        print(sch.attrs)
+    # emp_proj.closure(update=True)
+
+
+    # OneNFs = emp_proj.trans1NF()
+    # for sch in OneNFs:
+        # print(sch.attrs)
--- lab2/utils.py
+++ lab2/utils.py
@@ -2,20 +2,27 @@
 Author: Zhen Tong 120090694@link.cuhk.edu.cn
 '''
 
+import itertools
+from tqdm import tqdm
+
 
 class FunctionalDependency:
 
-    def __init__(self, master_attrs, slave):
-        self.master_attrs = master_attrs
-        self.slave = slave
+    def __init__(self, alpha:set, beta:set):
+        self.alpha = alpha
+        self.beta = beta
 
     def show(self):
-        for i, master in enumerate(self.master_attrs):
-            if i < len(self.master_attrs)-1:
-                print(master, end=", ")
+        for i, alpha in enumerate(self.alpha):
+            if i < len(self.alpha)-1:
+                print(alpha, end=", ")
             else :
-                print(master, end=" -> ")
-        print(self.slave)
+                print(alpha, end=" -> ")
+        for j, beta in enumerate(self.beta):
+            if j < len(self.beta)-1:
+                print(beta, end=", ")
+            else:
+                print(beta)
         
 
 class Schema:
@@ -24,15 +31,25 @@ class Schema:
         self.attrs = []
         self.pks = pks
         self.FDs = []
-        self.F_plus = None
+        self.F_plus = []
 
-    def add_FD(self, fd:FunctionalDependency):
-        for master in fd.master_attrs:
-            if master not in self.attrs:
-                self.attrs.append(master)
-        if fd.slave not in self.attrs:
-            self.attrs.append(fd.slave)
-        self.FDs.append(fd)
+    def add_FD(self, fd:FunctionalDependency, add2closure = False):
+        if self.exist_fd(fd, in_closure=add2closure):
+            return False
+        
+        for alpha in fd.alpha:
+            if alpha not in self.attrs:
+                self.attrs.append(alpha)
+
+        for beta in fd.beta:
+            if beta not in self.attrs:
+                self.attrs.append(beta)
+        
+        if add2closure:
+            self.F_plus.append(fd)
+        else:
+            self.FDs.append(fd)
+            
     
     def trans1NF(self):
         """
@@ -42,10 +59,10 @@ class Schema:
         for fd in self.FDs:
 
             # Check if the masters are all pks
-            if(is_subset(fd.master_attr, self.pks)):
+            if(is_subset(fd.alpha, self.pks)):
 
                 master_name = ""
-                for master in fd.master_attrs:
+                for master in fd.alpha:
                     master_name += master
                 fd.show()
                 if master_name in fd_kind_dict.keys():
@@ -56,7 +73,7 @@ class Schema:
             
             # Else, the msters have part not pks, find the master of the non-pks
             else:
-                non_pk_masters = subtract_set(fd.master_attrs, self.pks)
+                non_pk_masters = subtract_set(fd.alpha, self.pks)
                 new_masters = self.find_pks_masters(non_pk_masters) 
 
         
@@ -68,30 +85,89 @@ class Schema:
             oneNFs.append(sch)
         return oneNFs
 
-    def find_pks_masters(self, non_pk_attrs):
-        """
-        input: a list of non-pk attributes
-        output a list of [pk masters list] (2-nest list)
-        """
-        pk_masters_set = []
-        for m in non_pk_attrs:
-            pk_masters = []
-            for fd in self.closure():
-                if m == fd.slave and is_subset(fd.master_attrs, self.pks):
-                    pk_masters+=fd.master_attrs
-                    break
-            pk_masters_set.append(pk_masters)
-        return pk_masters_set
+    
+    def exist_fd(self, new_fd:FunctionalDependency, in_closure = False):
+        # find the fd in clousure
+        if in_closure:
+            for fd in self.F_plus:
+                if fd.alpha == new_fd.alpha and fd.beta == new_fd.beta:
+                    return True
+            else:
+                return False
+
+        # find the fd in FDs
+        else:
+            for fd in self.FDs:
+                if fd.alpha == new_fd.alpha and fd.beta == new_fd.beta:
+                    return True
+            else:
+                return False
+
             
-    def closure(self, update = False):
+    def closure(self, update = True):
         """
+        input: update = True when you want to compute the new closure again.
         output: the colsure list of it. 
         """
         
-        if self.F_plus == None or update:
-            # TODO get closure
+        if update==False:
+            return self.FDs
         
+        # TODO update closure
+        self.F_plus = []
+        # apply the reflexivity rule /* Generate all trivial dependencies */
+        # generate all the combination of attrs
+        all_combinations = []
+        for r in range(1, len(self.attrs) + 1):
+            all_combinations.extend(list(itertools.combinations(self.attrs, r)))
+
+        for comb_list in all_combinations:
+            sub_combination = []
+            for r in range(1, len(comb_list) + 1):
+                sub_combination.extend(list(itertools.combinations(comb_list, r)))
+            # print("subset size = ", len(sub_combination))
+            for beta in sub_combination:
+                # add the trivial to F_plus
+                fd = FunctionalDependency(comb_list, beta)
+                # fd.show()
+                self.add_FD(fd, add2closure=True)
+        
+        print("The Closure Size is: ", len(self.F_plus))
+
+        # apply the augmentation rule on f in F+
+        F_plus_copy= []
+        while len(F_plus_copy) != len(self.F_plus):
+            # update the copy
+            F_plus_copy= [fd for fd in self.F_plus]
+            for fd in F_plus_copy:
+                # apply the augmentation rule on f
+                alpha = fd.alpha
+                beta = fd.beta
+                for gamma in all_combinations:
+                    new_alpha = set(alpha).union(set(gamma))
+                    new_beta = set(beta).union(set(gamma))
+                    new_fd = FunctionalDependency(new_alpha, new_beta)
+                    self.add_FD(new_fd, add2closure=True)
+            print("After augmentation, the Closure Size is: ", len(self.F_plus))
+
+            # transitivity
+            all_pairs = list(itertools.combinations(self.F_plus, 2))
+            for pair in tqdm(all_pairs):
+                # print(pair)
+                f1, f2 = pair
+                if f1.beta == f2.alpha:
+                    new_fd = FunctionalDependency(f1.beta, f2.alpha)
+                    self.add_FD(new_fd, add2closure=True)
+                if f2.beta == f1.alpha:
+                    new_fd = FunctionalDependency(f2.beta, f1.alpha)
+                    self.add_FD(new_fd, add2closure=True)
+            print("After transitivity, the Closure Size is: ", len(self.F_plus))
+
+        print("finish with size = ", len(self.F_plus))
         return self.F_plus
+
+        
+        
 
 
 
